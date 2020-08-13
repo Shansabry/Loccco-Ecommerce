a5c4b953f0fd9afeeaf8ee020c0e99ff099e984c
Shanmuga Sabareesh <shansabry@gmail.com>
2020-08-13 18:57:22 +0530

    refs #00001 Ecommerce Initial Commit

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default  {
+    products: [
+        {
+            _id: 13444,
+            name: "Oppo Reno 3",
+            category: "mobile",
+            image: "/images/oppo-reno3.jpeg",
+            price: 23000,
+            brand: "Oppo",
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: 17564,
+            name: "Apple Iphone SE",
+            category: "mobile",
+            image: "/images/iphoneSE.jpg",
+            price: 42000,
+            brand: "Oppo",
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: 20111,
+            name: "Anker soundcore",
+            category: "earphone",
+            image: "/images/oppo-reno3.jpeg",
+            price: 10000,
+            brand: "Oppo",
+            rating: 4.5,
+            numReviews: 10
+        }
+    ]
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+npm i create-react-app frontend
+npm i @babel/cli @babel/core @babel/node @babel/preset-env nodemon // backend
+npm i @babel/cli @babel/core @babel/node @babel/preset-env nodemon --save-dev
+npm i axios // backend
+give proxy to frontend package to connect with backend
+npm i redux react-redux// frontend
